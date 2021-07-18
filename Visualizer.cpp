0907137e02197901233ba3846218599aa31189f2
lardira <danilkefimov@gmail.com>
2021-07-18 21:51:06 +0300

    changed structs to classes and added hints panel

--- Visualizer.cpp
+++ Visualizer.cpp
@@ -43,19 +43,19 @@ void clearRods(std::vector<sf::RectangleShape>& rods)
 }
 
 //easier and more readable way of storing algorithms info
-static struct Info
+class AlgorithmDescription
 {
 public:
-	Info() = default;
-	~Info() = default;
+	AlgorithmDescription() = default;
+	~AlgorithmDescription() = default;
 
-	Info(std::string type, size_t quantity)
+	AlgorithmDescription(std::string type, size_t quantity)
 	{
 		SetAlgorithmType(type);
 		SetQuantity(quantity);
 	}
 
-	Info(Algorithms::Type type, size_t quantity)
+	AlgorithmDescription(Algorithms::Type type, size_t quantity)
 	{
 
 		switch (type)
@@ -80,7 +80,7 @@ public:
 		SetQuantity(quantity);
 	}
 	//copy constructor
-	Info(Info& info)
+	AlgorithmDescription(AlgorithmDescription& info)
 		: algorithmType(info.GetAlgorithmType()), quantity(info.GetQuantity())
 	{
 	}
@@ -124,7 +124,7 @@ public:
 
 	void SetQuantity(size_t quantity)
 	{
-		Info::quantity = "Quantity: " + std::to_string(quantity);
+		AlgorithmDescription::quantity = "Quantity: " + std::to_string(quantity);
 	}
 
 	std::string GetQuantity() const
@@ -137,9 +137,59 @@ private:
 	std::string quantity;
 };
 
+class HintsPanel
+{
+public:
+	bool isEnabled = false;
+	sf::RectangleShape& panel;
+	sf::Text& sfText;
+
+private:
+	std::string text = 
+	{
+			"Tilda(~): Close Hints\n"
+			"Esc: Close Program\n"
+			"Space: Pause/Play\n"
+			"Up/Down: Change Speed\n"
+			"F1: Bubble Sort\n"
+			"F2: Insertion Sort\n"
+			"F3: Selection Sort\n"
+	};
+
+public:
+	HintsPanel(sf::RectangleShape& sfShape, sf::Text& sfText):
+		panel(sfShape), sfText(sfText)
+	{		
+		unsigned int charSize = 24;
+		sfShape.setSize(sf::Vector2f(15.f * charSize, 10.f * charSize));
+		//gray color rgba(149, 165, 166)
+		//alpha channel (200)
+		sfShape.setFillColor(sf::Color(149, 165, 166, 200));
+		sfText.setString(text);
+		sfText.setCharacterSize(charSize);
+		sfText.setFillColor(sf::Color::Green);
+	}
+
+	HintsPanel() = default;
+	~HintsPanel() = default;
+
+	void setPosition(sf::Vector2f& pos)
+	{
+		sfText.setPosition(pos);
+		panel.setPosition(pos);
+	}
+
+	void setPosition(float x, float y)
+	{
+		sf::Vector2f newPos = { x, y };
+		sfText.setPosition(newPos);
+		panel.setPosition(newPos);
+	}
+};
+
 void draw(std::vector<int>& arr, Algorithms::Type type)
 {
-	bool showing = false;
+	bool isShowing = false, isShowingHints = false;
 	const unsigned int screenSize[2] = { 1000, 1000 };
 	sf::RenderWindow window = { sf::VideoMode(screenSize[0], screenSize[1]), "Agorithms Visualizer" };
 
@@ -155,16 +205,18 @@ void draw(std::vector<int>& arr, Algorithms::Type type)
 	if (!font.loadFromFile("fonts/main_04b03b.ttf"))
 		return;
 
-	Info description(type, arr.size());
+	AlgorithmDescription description(type, arr.size());
 	sf::Text info(description.GetInfo(), font, 32);
-
-	std::string hintsText = "F1: bubble sort F2: insertion sort F3: selection sort\n";
-	hintsText = hintsText + "Space: play/pause";
-	sf::Text hints(hintsText, font, 24);
-
 	info.setFillColor(sf::Color::Green);
-	hints.setFillColor(sf::Color::Green);
-	hints.setPosition(sf::Vector2f((int)window.getSize().x / 2 - 100, 0));
+
+	sf::RectangleShape panelShape;
+	sf::Text hints;
+	HintsPanel hintsPanel(panelShape, hints);
+	hintsPanel.sfText.setFont(font);
+	hintsPanel.setPosition((float)window.getSize().x / 2, 0);
+
+	sf::Text howToShowHints(hints);
+	howToShowHints.setString("Press (~) to show hints");
 
 	fillRodsArray(rods, arr, window);
 
@@ -184,49 +236,54 @@ void draw(std::vector<int>& arr, Algorithms::Type type)
 			{
 				switch (event.key.code)
 				{
-				case sf::Keyboard::F1: 
-				case sf::Keyboard::F2: 
+				case sf::Keyboard::Tilde:
+					hintsPanel.isEnabled = !hintsPanel.isEnabled;
+					break;
+
+				case sf::Keyboard::F1:
+				case sf::Keyboard::F2:
 				case sf::Keyboard::F3:
-						type = (Algorithms::Type)(event.key.code - sf::Keyboard::F1);
-						idsCompared.first = 0;
-						idsCompared.second = 0;
-						step = 0;
-						arr = copyArray;
-						description.SetAlgorithmType(type);
-						info.setString(description.GetInfo());
+					type = (Algorithms::Type)(event.key.code - sf::Keyboard::F1);
+					idsCompared.first = 0;
+					idsCompared.second = 0;
+					step = 0;
+					arr = copyArray;
+					description.SetAlgorithmType(type);
+					info.setString(description.GetInfo());
 					break;
 
 				case sf::Keyboard::Space:
-					showing = !showing;
+					isShowing = !isShowing;
 					break;
 
 				case sf::Keyboard::Escape:
 					std::exit(1);
 					break;
 
-				case sf::Keyboard::Up:
-					timeToSLeep = timeToSLeep < 500 ? timeToSLeep + 50 : timeToSLeep;
+				case sf::Keyboard::Down:
+					timeToSLeep = timeToSLeep < 500 ? timeToSLeep + 25 : timeToSLeep;
 					break;
 
-				case sf::Keyboard::Down:
-					timeToSLeep = timeToSLeep - 50 > 0 ? timeToSLeep - 50 : 0;
+				case sf::Keyboard::Up:
+					timeToSLeep = timeToSLeep - 25 > 0 ? timeToSLeep - 25 : 0;
 					break;
 
 				default:
+					//TODO:throw an exception
 					break;
 				}
 			}
 		}
-		
+
+		//wait before proceeding for clearer visualization
 		std::this_thread::sleep_for(std::chrono::milliseconds(timeToSLeep));
+
+		//refreshing window for drawing new frame
 		window.clear();
-		//show info
-		window.draw(info);
-		window.draw(hints);
 
 		//calls sorting algorithm (step is required cuz it's using 
 		// a sorting algorithm in a while loop
-		if (step < arr.size() && showing)
+		if (step < arr.size() && isShowing)
 		{
 			switch (type)
 			{
@@ -253,6 +310,19 @@ void draw(std::vector<int>& arr, Algorithms::Type type)
 		step < rods.size() ? colorRods(rods, idsCompared) : clearRods(rods);
 		//draws updated rods
 		drawRods(rods, arr, window);
+		//drawing algorithm description
+		window.draw(info);
+		//drawing hints panel
+		if (hintsPanel.isEnabled)
+		{
+			window.draw(hintsPanel.panel);
+			window.draw(hintsPanel.sfText);
+		}
+		else
+		{
+			window.draw(howToShowHints);
+		}
+		//displays everything that has been drawn to the window
 		window.display();
 	}
 }
